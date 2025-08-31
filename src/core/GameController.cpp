0bb9fc8dfaf3de7626bddf21664530f6f638756d
Piotr Suchy <piotrsuchy@proton.me>
2025-08-31 20:48:08 +0200

    fix: brackets

--- src/core/GameController.cpp
+++ src/core/GameController.cpp
@@ -65,13 +65,13 @@ void GameController::processEvents()
     sf::Event event;
     while (window.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed) {
+        if (event.type == sf::Event::Closed)
+        {
             window.close();
         }
 
         // forward everything else to the input system
         inputManager.handleEvent(event);
-
     }
 }
 
